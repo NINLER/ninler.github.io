8a9e802a43ad24ff9ea0f0bcd4a573ada77b7d1a
Quiet_blue <96299502+NINLER@users.noreply.github.com>
2022-09-14 21:26:14 +0800

    Add files via upload

--- js/top-box-color.js
+++ js/top-box-color.js
@@ -1,33 +1,34 @@
-let lv=127,rv=205;
+let lv=127,rv=200;
 let r=lv,g=rv,b=lv;
 let rs=1,gs=1,bs=1;
-function change()
+function change(time)
 {
-    r+=(Math.random()*rs*20);
+    r+=(Math.random()*rs*0.1*time);
     if(r>rv||r<lv)
     {
         r=Math.min(r,rv);
         r=Math.max(r,lv);
         rs=-rs;
     }
-    g+=(Math.random()*gs*20);
+    g+=(Math.random()*gs*0.1*time);
     if(g>rv||g<lv)
     {
         g=Math.min(g,rv);
         g=Math.max(g,lv);
         gs=-gs;
     }
-    b+=(Math.random()*bs*20);
+    b+=(Math.random()*bs*0.1*time);
     if(b>rv||b<lv)
     {
         b=Math.min(b,rv);
         b=Math.max(b,lv);
         bs=-bs;
     }
-    let temp=document.getElementById("head-box");
-    temp.style="background:rgb("+r+","+g+","+b+")";
-    console.log(temp.style.background);
-    console.log(r+' '+g+" "+b);
+    let temp=document.getElementById("top");
+    // console.log(temp.style.background);
+    // console.log(r+' '+g+" "+b);
+    // console.log(temp);
+    temp.style.backgroundColor="rgb("+r+","+g+","+b+")";
     return;
 }
-window.setInterval(change,200);
+window.setInterval("change(10)",10);
