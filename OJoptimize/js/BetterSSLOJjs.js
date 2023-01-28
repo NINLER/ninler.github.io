d76d0923c5b0778ca8b6ff8cc2329bbd8192f08b
Quiet_blue <96299502+NINLER@users.noreply.github.com>
2023-01-28 17:03:00 +0800

    Update BetterSSLOJjs.js

--- OJoptimize/js/BetterSSLOJjs.js
+++ OJoptimize/js/BetterSSLOJjs.js
@@ -1,2 +1,58 @@
-var BetterSSLOJTempermonkeyCode='var id;\nvar tempTimeout;\nfunction work()\n{\n    let temp1=document.getElementsByClassName("font-content");\n    let example=temp1[3].firstChild;\n    let children=example.children;\n    let size=example.childElementCount;\n    console.log(children[1].firstChild.firstChild.firstChild.innerText);\n    console.log(size);\n    let added="";\n    // example.innerHTML="";\n    for(let i=1; i<=size/2; i++)\n    {\n        added+=\'<p>\'+children[i*2-2].innerHTML+\'</p>\';\n        // Button\n        added+=\'<button id="copy\'+i+\'" onclick="copy(\'+i+\')" style="height:20pt;font-family:consolas;background-color:#000000;color:#38bfff;width:auto;border-radius:10pt;text-ailgn:center;">Copy Text</button>\'\n        added+=\'<div id="text\'+i+\'"class="ui existing segment">\'+children[i*2-1].innerHTML+\'</div>\';\n    }\n    example.innerHTML=added;\n    return;\n}\nwork();\nfunction copy(i)\n{\n    // console.log("try to copy para."+i);\n    id="copy"+i;\n    // console.log(btn);\n    const input=document.createElement(\'textarea\');\n    let text=document.getElementById(\'text\'+i).firstChild.firstChild.firstChild.innerText;\n    input.value=text;\n    document.body.appendChild(input);\n    input.select();\n    document.execCommand(\'copy\');\n    document.body.removeChild(input);\n    document.getElementById(id).innerText="Successfully";\n    document.getElementById(id).style.color="lightgreen";\n    setTimeout(\'tempTimeout=document.getElementById(id);tempTimeout.innerText="Copy Text";tempTimeout.style.color="#38bfff";\',1000);\n    // console.log("Copy Successfully.");\n    return;\n}\n'
+var BetterSSLOJTempermonkeyCode=`// ==UserScript==
+// @name         Better SSLOJ
+// @namespace    http://tampermonkey.net/
+// @version      1.1
+// @author       You
+// @match        http://noip.ybtoj.com.cn/*
+// @match        http://ssloj.cn/*
+// @grant        none
+// ==/UserScript==
 
+(function() {
+    'use strict';
+    // Your code here...
+    var id;
+    var copied;
+    var tempTimeout;
+    function work()
+    {
+        let temp1=document.getElementsByClassName("font-content");
+        let example=temp1[3].firstChild;
+        let children=example.children;
+        let size=example.childElementCount;
+        let added="";
+        // example.innerHTML="";
+        copied=new Array(size/2);
+        for(let i=1; i<=size/2; i++)
+        {
+            added+='<p>'+children[i*2-2].innerHTML+'</p>';
+            // Button
+            added+='<button id="copy'+i+'" style="border-color:black !important;height:20pt;font-family:consolas;background-color:#000000 !important;color:#38bfff !important;width:auto !important;border-radius:10pt !important;text-ailgn:center;">Copied Times:0</button>'
+            added+='<div id="text'+i+'"class="ui existing segment">'+children[i*2-1].innerHTML+'</div>';
+            copied[i]=0;
+        }
+        example.innerHTML=added;
+        return;
+    }
+    work();
+    document.onclick=function()
+    {
+        // console.log("try to copy para."+i);
+        let tar=event.target;
+        let i=tar.id;
+        i=i.substr(4);
+        console.log(i);
+        id="copy"+i;
+        copied[i]+=1;
+        const input=document.createElement('textarea');
+        let text=document.getElementById('text'+i).firstChild.firstChild.firstChild.innerText;
+        input.value=text;
+        document.body.appendChild(input);
+        input.select();
+        document.execCommand('copy');
+        document.body.removeChild(input);
+        document.getElementById(id).innerText="Copied Times:"+copied[i];
+        // console.log("Copy Successfully.");
+        return;
+    }
+})();`;
