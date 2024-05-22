2d2d281ccac803eaa6a833a8077b02b2726c2b01
静谧幽蓝 <1121659820@qq.com>
2024-05-22 18:01:55 +0800

    Updated homepage.

--- assets/js/renders.js
+++ assets/js/renders.js
@@ -19,13 +19,13 @@ $.render=()=>{
 
 $.loadWebsites=()=>{
     const data=[
-        {name:"神奇迷宫验证码",info:"疑似人身攻击",image:"./assets/img/7.png"},
-        {name:"网站解谜",info:"好吧有人已经通关了",image:"./assets/img/13.png"},
-        {name:"博弈论",info:"爆算 SG 函数",image:"./assets/img/5.png"},
-        {name:"网站资源",info:"让你的网站更美观便利",image:"./assets/img/6.png"},
-        {name:"Numble",info:"Wordle 的变种",image:"./assets/img/9.png"},
+        {name:"神奇迷宫验证码",info:"疑似人身攻击",image:"./assets/img/7.png",address:"./maze-captcha/maze-captcha.html"},
+        {name:"网站解谜",info:"好吧有人已经通关了",image:"./assets/img/13.png",address:"./puzzle/problem/problem1.html"},
+        {name:"博弈论",info:"爆算 SG 函数",image:"./assets/img/5.png",address:"./Game134/index.html"},
+        {name:"网站资源",info:"让你的网站更美观便利",image:"./assets/img/6.png",address:"./OJoptimize/index.html"},
+        {name:"Numble",info:"Wordle 的变种",image:"./assets/img/9.png",address:"./Numble/index.html"},
     ];
-    const template=(name,info,img,wid)=>{
+    const template=(name,info,img,wid,address)=>{
         return `
             <div class="col-lg-${wid}">
                 <div class="card bg-dark overlay overlay-black text-white shadow-sm border-0">
@@ -34,7 +34,11 @@ $.loadWebsites=()=>{
                         <div class="card-body">
                             <h3 class="card-title">${name}</h3>
                             <p class="card-text">${info}</p>
-                            <a href="${address}" class="btn btn-info btn-round">Visit website</a>
+                            ${
+                                address? 
+                                `<a href="${address}" class="btn btn-info btn-round">Visit website</a>`:
+                                `<div class="btn btn-dark" disabled style="cursor:not-allowed">No link</div>`
+                            }
                         </div>
                     </div>
                 </div>
@@ -44,8 +48,8 @@ $.loadWebsites=()=>{
     for(let i=0; i<data.length; i+=2)
     {
         let temp=``;
-        if(!data[i+1]) temp=template(data[i].name,data[i].info,data[i].image,6);
-        else temp=template(data[i].name,data[i].info,data[i].image,6)+template(data[i+1].name,data[i+1].info,data[i+1].image,6);
+        if(!data[i+1]) temp=template(data[i].name,data[i].info,data[i].image,6,data[i].address);
+        else temp=template(data[i].name,data[i].info,data[i].image,6,data[i].address)+template(data[i+1].name,data[i+1].info,data[i+1].image,6,data[i+1].address);
         temp='<section data-aos="fade-in" ptop=20 class="align-items-center" data-aos-offset=100><div class="row">'+temp+'</div></section>';
         origin.innerHTML+=temp;
     }
