ea75fdf381a2476879ad98041f759614b6d872b6
静谧幽蓝 <1121659820@qq.com>
2025-03-17 12:36:26 +0800

    Updated base64 png.

--- assets/js/renders.js
+++ assets/js/renders.js
@@ -19,18 +19,17 @@ $.render=()=>{
 
 $.loadWebsites=()=>{
     const data=[
-        {name:"神奇迷宫验证码",info:"疑似人身攻击",image:"https://s21.ax1x.com/2024/09/10/pAm08bR.jpg",address:"./maze-captcha/maze-captcha.html"},
-        {name:"网站解谜",info:"好吧有人已经通关了",image:"https://s21.ax1x.com/2024/09/10/pAm0M2F.png",address:"./puzzle/problem/problem1.html"},
-        {name:"博弈论",info:"爆算 SG 函数",image:"https://s21.ax1x.com/2024/09/10/pAm03r9.webp",address:"./Game134/index.html"},
-        {name:"网站资源",info:"让你的网站更美观便利",image:"https://s21.ax1x.com/2024/09/10/pAm0JV1.png",address:"./OJoptimize/index.html"},
-        {name:"Numble",info:"Wordle 的变种",image:"https://s21.ax1x.com/2024/09/10/pAm0uCT.webp",address:"./Numble/index.html"},
+        {name:"神奇迷宫验证码",info:"疑似人身攻击",image:"link1",address:"./maze-captcha/maze-captcha.html"},
+        {name:"网站解谜",info:"好吧有人已经通关了",image:"link2",address:"./puzzle/problem/problem1.html"},
+        {name:"博弈论",info:"爆算 SG 函数",image:"link3",address:"./Game134/index.html"},
+        {name:"网站资源",info:"让你的网站更美观便利",image:"link4",address:"./OJoptimize/index.html"},
+        {name:"Numble",info:"Wordle 的变种",image:"link5",address:"./Numble/index.html"},
     ];
     const template=(name,info,img,wid,address)=>{
         return `
             <div class="col-lg-${wid}">
                 <div class="card bg-dark overlay overlay-black text-white shadow-sm border-0">
-                    <img class="card-img rounded-sm" src="${img}" alt="Card image">
-                    <div class="card-img-overlay d-flex align-items-center text-center">
+                    <div class="card-img-overlay d-flex align-items-center text-center bg-img" src="${img}">
                         <div class="card-body">
                             <h3 class="card-title">${name}</h3>
                             <p class="card-text">${info}</p>
