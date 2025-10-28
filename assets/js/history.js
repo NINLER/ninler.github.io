9becfc05e5085c3e1b4c1ffaf0a014eb0fe1e08d
静谧幽蓝 <1121659820@qq.com>
2025-10-28 13:31:14 +0800

    ver2.0

---
+++ assets/js/history.js
@@ -0,0 +1,20 @@
+const HistoryInfo=[
+    {version: "2.0", info: "· 添加了历史记录栏。"},
+    {version: "1.0", info: "· 创建了这个网站的文件夹！<br>· 1.0 的其他更新记录消失了喵！"}
+];
+
+(()=>{
+    $.loadHistory=()=>{
+        const elem=document.querySelector(".history-content");
+        for(let it of HistoryInfo)
+        {
+            let text=`
+                <h5>${it.version}</h5>
+                <hr style="margin: 5px 0">
+                <p>${it.info}</p>
+            `;
+            elem.innerHTML+=text;
+        }
+        return;
+    }
+})();
--- assets/js/index.js
+++
@@ -1,3 +0,0 @@
-(()=>{
-    $.loadWebsites(); $.render();
-})()
