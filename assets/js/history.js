0b9f11745ff032c3a02b0eb6d2530d3327ad15e4
NINLER <grey_yyds@foxmail.com>
2025-10-29 18:11:45 +0800

    Fixed.

--- assets/js/history.js
+++ assets/js/history.js
@@ -1,7 +1,18 @@
 const HistoryInfo=[
-    {version: "2.1", info: "· 将 Game 134 重修了一遍。"},
-    {version: "2.0", info: "· 添加了历史记录栏。"},
-    {version: "1.0", info: "· 创建了这个网站的文件夹！<br>· 1.0 的其他更新记录消失了喵！"}
+    {version: "2.2", info: "· 将 Game 134 重修了一遍。<br>· 找到了之前的历史更新记录喵！"},
+    {version: "2.1", info: "· 添加了历史记录栏。<br>· 将 博弈论 改名为 Game 134。"},
+    {version: "2.0", info: "· 网站主页重写了一遍喵！"},
+    {version: "1.6.7", info: "· 添加 Numble 模块。"},
+    {version: "1.6.3", info: "· 将 自己做的解密 改名为 网站解谜。<br>· 在 网站解谜 中加入了 防跳关系统。"},
+    {version: "1.6.2", info: "· 在 主页面 添加了背景图片，取代了之前的 随机颜色背景。"},
+    {version: "1.6.1", info: "· 更改了 自己做的解密 板块中的副标题。"},
+    {version: "1.6", info: "· 在 神奇迷宫验证码 中加入了个人记录！"},
+    {version: "1.5", info: "· 添加 Update History 模块。"},
+    {version: "1.4", info: "· 添加 网站优化 板块。"},
+    {version: "1.3", info: "· 添加 博弈论 板块。"},
+    {version: "1.2", info: "· 添加 自己做的解密 板块。"},
+    {version: "1.1", info: "· 添加 神奇迷宫验证码 板块。"},
+    {version: "1.0", info: "· 创建了这个网站的文件夹！"}
 ];
 
 (()=>{
